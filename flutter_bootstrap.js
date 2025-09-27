ab657a469eed6f8e3073bf942f644f97bfdd28f2
adwayaghor <adway.aghor23@pccoepune.org>
2025-09-27 20:33:59 +0530

    Updated Projects

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3652873067"
+    serviceWorkerVersion: "2923566330"
   }
 });
